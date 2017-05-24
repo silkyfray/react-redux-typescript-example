ecb6475f555bb68207125e3c37f9f805d7c68d8b
Antonio Nikolov <anikolovn@gmail.com>
2017-05-24 23:10:03 +0100

    -more test data

--- server/insertTestData.js
+++ server/insertTestData.js
@@ -1,7 +1,9 @@
 var http = require("http");
 var rp = require("request-promise");
 
-var testData = [
+var constants = require("../shared/apiEndpoints")
+
+var submitData = [
     "http://www.carolynparker.com/",
     "http://kube.frederiksberg.dk/",
     "http://www.futuremobility.se/",
@@ -12,24 +14,121 @@ var testData = [
     "http://www.playinglynch.com/",
     "https://justcoded.com/",
     "https://www.krealid.com/",
-    "https://madebyfalcon.co.uk/"
-    ]
+    "https://madebyfalcon.co.uk/",
+    "www.google.co.uk",
+    "www.linkedin.com",
+    "www.google.fr",
+    "www.google.ru",
+    "www.list.tmall.com",
+    "www.google.com.br",
+    "www.yandex.ru",
+    "www.google.com.hk",
+    "www.yahoo.co.jp",
+    "www.netflix.com",
+    "www.google.it",
+    "www.t.co",
+    "www.ntd.tv",
+    "www.imgur.com",
+    "www.google.es",
+    "www.ebay.com",
+    "www.onclkds.com",
+    "www.hao123.com",
+    "www.bing.com",
+    "www.pornhub.com",
+    "www.msn.com",
+    "www.microsoft.com",
+    "www.wordpress.com",
+    "www.google.ca",
+    "www.livejasmin.com",
+    "www.twitch.tv",
+    "www.aliexpress.com",
+    "www.google.co.uk",
+    "www.linkedin.com",
+    "www.google.fr",
+    "www.google.ru",
+    "www.list.tmall.com",
+    "www.google.com.br",
+    "www.yandex.ru",
+    "www.google.com.hk",
+    "www.yahoo.co.jp",
+    "www.netflix.com",
+    "www.google.it",
+    "www.t.co",
+    "www.ntd.tv",
+    "www.imgur.com",
+    "www.google.es",
+    "www.ebay.com",
+    "www.onclkds.com",
+    "www.hao123.com",
+    "www.bing.com",
+    "www.pornhub.com",
+    "www.msn.com",
+    "www.microsoft.com",
+    "www.wordpress.com",
+    "www.google.ca",
+    "www.livejasmin.com",
+    "www.twitch.tv",
 
-testData.forEach(function(design) {
+]
+
+let approvedData = [
+    "www.google.co.uk",
+    "www.linkedin.com",
+    "www.google.fr",
+    "www.google.ru",
+    "www.list.tmall.com",
+    "www.google.com.br",
+    "www.yandex.ru",
+    "www.google.com.hk",
+    "www.yahoo.co.jp",
+    "www.netflix.com",
+    "www.google.it",
+    "www.t.co",
+    "www.ntd.tv",
+    "www.imgur.com",
+    "www.google.es",
+    "www.ebay.com",
+    "www.onclkds.com",
+    "www.hao123.com",
+    "www.bing.com",
+    "www.pornhub.com",
+    "www.msn.com",
+    "www.microsoft.com",
+    "www.wordpress.com",
+    "www.google.ca",
+    "www.livejasmin.com",
+    "www.twitch.tv",
+    "www.aliexpress.com"
+]
+
+// submitData.forEach(function (design) {
+//     var options = {
+//         method: 'POST',
+//         uri: "http://localhost:3001" + constants.kApiSubmitDesign,
+//         body: {
+//             designUrl: design,
+//             title: "This website looks really good",
+//             description: "Some description"
+//         },
+//         json: true // Automatically stringifies the body to JSON
+//     };
+//     console.log("posting: " + design);
+//     rp.post(options)
+//         .then(response => console.log(response))
+//         .catch(response => console.log(response));
+// })
+
+approvedData.forEach(function (design) {
     var options = {
         method: 'POST',
-        uri: "http://localhost:3001/api/design",
+        uri: "http://localhost:3001" + constants.kApiApproveDesign,
         body: {
             designUrl: design,
-            title: "This website looks really good",
-            description: "Some description"
-         },
+        },
         json: true // Automatically stringifies the body to JSON
     };
     console.log("posting: " + design);
-     rp.post(options)
-     .then(response => console.log(response))
-     .catch(response => console.log(response));
+    rp.post(options)
+        .then(response => console.log(response))
+        .catch(response => console.log(response));
 })
-
-
--- shared/apiEndpoints.js
+++ shared/apiEndpoints.js
@@ -1,3 +1,5 @@
 exports = module.exports
 
+exports.kApiSubmitDesign = "/api/design/submit"
+exports.kApiApproveDesign = "/api/design/approve"
 exports.kApiReadApprovals = "/api/approval"
